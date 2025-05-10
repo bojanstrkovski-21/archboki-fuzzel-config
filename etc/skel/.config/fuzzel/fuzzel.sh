0d8dc860eee68b0a7c9d577470ad0b8eaec34f10
bojanstrkovski-21 <bojanstrkovski.21@gmail.com>
2025-05-10 12:30:55 +0200

    add fuzzel-config

---
+++ etc/skel/.config/fuzzel/fuzzel.sh
@@ -0,0 +1,17 @@
+
+fuzzel --config=$HOME/Documents/fuzzel/caughtwuick-fuzzel.ini
+fuzzel --config=$HOME/Documents/fuzzel/dangooddd-fuzzel.ini
+fuzzel --config=$HOME/Documents/fuzzel/frestein-fuzzel.ini
+fuzzel --config=$HOME/Documents/fuzzel/frestein2fuzzel.ini
+fuzzel --config=$HOME/Documents/fuzzel/fuzzel-cyberpunk.ini
+fuzzel --config=$HOME/Documents/fuzzel/fuzzel-gruvbox.ini
+fuzzel --config=$HOME/Documents/fuzzel/fuzzel.ini
+fuzzel --config=$HOME/Documents/fuzzel/gitclonespecfolfer
+fuzzel --config=$HOME/Documents/fuzzel/hunteros-fuzzel.ini
+fuzzel --config=$HOME/Documents/fuzzel/ivncnylmz-fuzzel.ini
+fuzzel --config=$HOME/Documents/fuzzel/jacksinneverfirest-fuzzel.ini
+fuzzel --config=$HOME/Documents/fuzzel/jxbpfuzzel.ini
+fuzzel --config=$HOME/Documents/fuzzel/ludovico-fuzzel.ini
+fuzzel --config=$HOME/Documents/fuzzel/mhkarimi1383-fuzzel.ini
+fuzzel --config=$HOME/Documents/fuzzel/tak1nfuzzel.ini
+fuzzel --config=$HOME/Documents/fuzzel/weguy1fuzzel.ini
